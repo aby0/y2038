torvalds/linux

commit bd471258f2e0911f29d5dc3e1689de35518a157a

staging: android: logger: use kuid_t instead of uid_t
Use kuid_t instead of uid_t, to pass the UIDGID_STRICT_TYPE_CHECKS.

Signed-off-by: Xiong Zhou <jencce.kernel@gmail.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/android/logger.c
@@ -242,7 +242,7 @@ static ssize_t do_read_log_to_user(struct logger_log *log,
  * 'log->buffer' which contains the first entry readable by 'euid'
  */
 static size_t get_next_entry_by_uid(struct logger_log *log,
-		size_t off, uid_t euid)
+		size_t off, kuid_t euid)
 {
 	while (off != log->w_off) {
 		struct logger_entry *entry;
@@ -251,7 +251,7 @@ static size_t get_next_entry_by_uid(struct logger_log *log,
 
 		entry = get_entry_header(log, off, &scratch);
 
-		if (entry->euid == euid)
+		if (uid_eq(entry->euid, euid))
 			return off;
 
 		next_len = sizeof(struct logger_entry) + entry->len;

drivers/staging/android/logger.h
@@ -66,7 +66,7 @@ struct logger_entry {
 	__s32		tid;
 	__s32		sec;
 	__s32		nsec;
-	uid_t		euid;
+	kuid_t		euid;
 	char		msg[0];
 };
 

aby0/y2038

commit 275298e50a87f136274982b6436f6129d0924822

Staging: android: Fix the android logger time format
logger_entry uses 32-bit integers to store time and 'logger_aio_write()'
uses 'timeval' to get current time.

32-bit systems using 'struct timeval' will break in the year 2038,
so we have to replace that code with more appropriate types.

A new version for logger entry - 'struct logger_entry_v3' is created.
So now, we have 3 versions of logger entry:
 * version 1: struct user_logger_entry_compat
 * version 2: struct logger_entry
 * version 3: struct logger_entry_v3

New version architecture:
 * Most architecture align struct members according to their size, so in
   particular a 64-bit member will start on a multiple of 64-bits (8 bytes).
   we have 12 bytes before the 'sec' member, so the compiler will add
   four bytes of padding between tid and sec. We don't want that, so we move
   euid before sec to avoid the padding.

 *'nsec' is kept as 32 bit because it will never be more than
   NSEC_PER_SEC, which fits into a 32-bit number.

In drivers/staging/android/TODO file,the comment about a pre-existing bug
data structure definition is added. This bug was evidently introduced by
Xiong Zhou in the patch bd471258f2e09 ("staging: android: logger:
use kuid_t instead of uid_t") where the code was changed to hide
a valid build warning instead of solving the bug that was identified
by it.

Version 3 uses 64-bit integers to store time. 'struct timeval' is
replaced by 'timespec64'.

The driver currently uses 'current_kernel_time()', which is the
'coarse resolution', but that does not yet have a 64-bit equivalent,
neither ktime_t nor timespec64. So we have used 'ktime_get_real_ts64' as
it is useful for reporting absolute time to users but not as fast as
'current_kernel_time()'.

drivers/staging/android/TODO
@@ -5,6 +5,13 @@ TODO:
 	- make sure things build as modules properly
 	- add proper arch dependencies as needed
 	- audit userspace interfaces to make sure they are sane
+	- kuid_t should never be exposed to user space as it is
+	  kernel internal type. Data structure for this kuid_t is:
+	   typedef struct {
+	   		uid_t val;
+       } kuid_t;
+    - This bug is introduced by Xiong Zhou in the patch bd471258f2e09
+      ("staging: android: logger: use kuid_t instead of uid_t")
 
 Please send patches to Greg Kroah-Hartman <greg@kroah.com> and Cc:
 Brian Swetland <swetland@google.com>

drivers/staging/android/logger.c
@@ -27,6 +27,7 @@
 #include <linux/poll.h>
 #include <linux/slab.h>
 #include <linux/time.h>
+#include <linux/ktime.h>
 #include <linux/vmalloc.h>
 #include <linux/aio.h>
 #include "logger.h"
@@ -115,25 +116,25 @@ static inline struct logger_log *file_get_log(struct file *file)
 }
 
 /*
- * get_entry_header - returns a pointer to the logger_entry header within
- * 'log' starting at offset 'off'. A temporary logger_entry 'scratch' must
+ * get_entry_header - returns a pointer to the logger_entry_v3 header within
+ * 'log' starting at offset 'off'. A temporary logger_entry_v3 'scratch' must
  * be provided. Typically the return value will be a pointer within
  * 'logger->buf'.  However, a pointer to 'scratch' may be returned if
  * the log entry spans the end and beginning of the circular buffer.
  */
-static struct logger_entry *get_entry_header(struct logger_log *log,
-		size_t off, struct logger_entry *scratch)
+static struct logger_entry_v3 *get_entry_header(struct logger_log *log,
+		size_t off, struct logger_entry_v3 *scratch)
 {
-	size_t len = min(sizeof(struct logger_entry), log->size - off);
+	size_t len = min(sizeof(struct logger_entry_v3), log->size - off);
 
-	if (len != sizeof(struct logger_entry)) {
+	if (len != sizeof(struct logger_entry_v3)) {
 		memcpy(((void *) scratch), log->buffer + off, len);
 		memcpy(((void *) scratch) + len, log->buffer,
-			sizeof(struct logger_entry) - len);
+			sizeof(struct logger_entry_v3) - len);
 		return scratch;
 	}
 
-	return (struct logger_entry *) (log->buffer + off);
+	return (struct logger_entry_v3 *) (log->buffer + off);
 }
 
 /*
@@ -148,8 +149,8 @@ static struct logger_entry *get_entry_header(struct logger_log *log,
  */
 static __u32 get_entry_msg_len(struct logger_log *log, size_t off)
 {
-	struct logger_entry scratch;
-	struct logger_entry *entry;
+	struct logger_entry_v3 scratch;
+	struct logger_entry_v3 *entry;
 
 	entry = get_entry_header(log, off, &scratch);
 	return entry->len;
@@ -159,15 +160,18 @@ static size_t get_user_hdr_len(int ver)
 {
 	if (ver < 2)
 		return sizeof(struct user_logger_entry_compat);
-	return sizeof(struct logger_entry);
+	if (ver < 3)
+		return sizeof(struct logger_entry);
+	return sizeof(struct logger_entry_v3);
 }
 
-static ssize_t copy_header_to_user(int ver, struct logger_entry *entry,
+static ssize_t copy_header_to_user(int ver, struct logger_entry_v3 *entry,
 					 char __user *buf)
 {
 	void *hdr;
 	size_t hdr_len;
 	struct user_logger_entry_compat v1;
+	struct logger_entry v2;
 
 	if (ver < 2) {
 		v1.len      = entry->len;
@@ -178,9 +182,20 @@ static ssize_t copy_header_to_user(int ver, struct logger_entry *entry,
 		v1.nsec     = entry->nsec;
 		hdr         = &v1;
 		hdr_len     = sizeof(struct user_logger_entry_compat);
+	} else if (ver < 3) {
+		v2.len      = entry->len;
+		v2.hdr_size = entry->hdr_size;
+		v2.pid      = entry->pid;
+		v2.tid      = entry->tid;
+		v2.sec      = entry->sec;
+		v2.nsec     = entry->nsec;
+		v2.euid     = entry->euid;
+		hdr         = &v2;
+		hdr_len     = sizeof(struct logger_entry);
+
 	} else {
 		hdr         = entry;
-		hdr_len     = sizeof(struct logger_entry);
+		hdr_len     = sizeof(struct logger_entry_v3);
 	}
 
 	return copy_to_user(buf, hdr, hdr_len);
@@ -197,8 +212,8 @@ static ssize_t do_read_log_to_user(struct logger_log *log,
 				   char __user *buf,
 				   size_t count)
 {
-	struct logger_entry scratch;
-	struct logger_entry *entry;
+	struct logger_entry_v3 scratch;
+	struct logger_entry_v3 *entry;
 	size_t len;
 	size_t msg_start;
 
@@ -213,7 +228,7 @@ static ssize_t do_read_log_to_user(struct logger_log *log,
 	count -= get_user_hdr_len(reader->r_ver);
 	buf += get_user_hdr_len(reader->r_ver);
 	msg_start = logger_offset(log,
-		reader->r_off + sizeof(struct logger_entry));
+		reader->r_off + sizeof(struct logger_entry_v3));
 
 	/*
 	 * We read from the msg in two disjoint operations. First, we read from
@@ -233,7 +248,7 @@ static ssize_t do_read_log_to_user(struct logger_log *log,
 			return -EFAULT;
 
 	reader->r_off = logger_offset(log, reader->r_off +
-		sizeof(struct logger_entry) + count);
+		sizeof(struct logger_entry_v3) + count);
 
 	return count + get_user_hdr_len(reader->r_ver);
 }
@@ -246,8 +261,8 @@ static size_t get_next_entry_by_uid(struct logger_log *log,
 		size_t off, kuid_t euid)
 {
 	while (off != log->w_off) {
-		struct logger_entry *entry;
-		struct logger_entry scratch;
+		struct logger_entry_v3 *entry;
+		struct logger_entry_v3 scratch;
 		size_t next_len;
 
 		entry = get_entry_header(log, off, &scratch);
@@ -255,7 +270,7 @@ static size_t get_next_entry_by_uid(struct logger_log *log,
 		if (uid_eq(entry->euid, euid))
 			return off;
 
-		next_len = sizeof(struct logger_entry) + entry->len;
+		next_len = sizeof(struct logger_entry_v3) + entry->len;
 		off = logger_offset(log, off + next_len);
 	}
 
@@ -350,7 +365,7 @@ static size_t get_next_entry(struct logger_log *log, size_t off, size_t len)
 	size_t count = 0;
 
 	do {
-		size_t nr = sizeof(struct logger_entry) +
+		size_t nr = sizeof(struct logger_entry_v3) +
 			get_entry_msg_len(log, off);
 		off = logger_offset(log, off + nr);
 		count += nr;
@@ -471,11 +486,11 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 {
 	struct logger_log *log = file_get_log(iocb->ki_filp);
 	size_t orig;
-	struct logger_entry header;
-	struct timespec now;
+	struct logger_entry_v3 header;
+	struct timespec64 now;
 	ssize_t ret = 0;
 
-	now = current_kernel_time();
+	ktime_get_real_ts64(&now);
 
 	header.pid = current->tgid;
 	header.tid = current->pid;
@@ -483,7 +498,7 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	header.nsec = now.tv_nsec;
 	header.euid = current_euid();
 	header.len = min_t(size_t, iocb->ki_nbytes, LOGGER_ENTRY_MAX_PAYLOAD);
-	header.hdr_size = sizeof(struct logger_entry);
+	header.hdr_size = sizeof(struct logger_entry_v3);
 
 	/* null writes succeed, return zero */
 	if (unlikely(!header.len))
@@ -499,9 +514,9 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	 * because if we partially fail, we can end up with clobbered log
 	 * entries that encroach on readable buffer.
 	 */
-	fix_up_readers(log, sizeof(struct logger_entry) + header.len);
+	fix_up_readers(log, sizeof(struct logger_entry_v3) + header.len);
 
-	do_write_log(log, &header, sizeof(struct logger_entry));
+	do_write_log(log, &header, sizeof(struct logger_entry_v3));
 
 	while (nr_segs-- > 0) {
 		size_t len;
@@ -647,7 +662,7 @@ static long logger_set_version(struct logger_reader *reader, void __user *arg)
 	if (copy_from_user(&version, arg, sizeof(int)))
 		return -EFAULT;
 
-	if ((version < 1) || (version > 2))
+	if ((version < 1) || (version > 3))
 		return -EINVAL;
 
 	reader->r_ver = version;

drivers/staging/android/logger.h
@@ -69,6 +69,31 @@ struct logger_entry {
 	kuid_t		euid;
 	char		msg[0];
 };
+/**
+ * struct logger_entry_v3 - defines a single entry that is given to a logger
+ * @len:	The length of the payload
+ * @hdr_size:	sizeof(struct logger_entry_v3)
+ * @pid:	The generating process' process ID
+ * @tid:	The generating process' thread ID
+ * @euid:	Effective UID of logger
+ * @sec:	The number of seconds that have elapsed since the Epoch
+ * @nsec:	The number of nanoseconds
+ * @msg:	The message that is to be logged
+ * The structure for version 3 of the logger_entry ABI.
+ * This structure is returned to userspace if ioctl(LOGGER_SET_VERSION)
+ * is called with version = 3
+ */
+struct logger_entry_v3 {
+	__u16		len;
+	__u16		hdr_size;
+	__s32		pid;
+	__s32		tid;
+	kuid_t		euid;
+	__s64		sec;
+	__s32		nsec;
+	char		msg[0];
+};
+
 
 #define LOGGER_LOG_RADIO	"log_radio"	/* radio-related messages */
 #define LOGGER_LOG_EVENTS	"log_events"	/* system/hardware events */